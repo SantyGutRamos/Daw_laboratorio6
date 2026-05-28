a5d863387ad02ad8d41e97c5b92674956a4dd608
Santiago Cristopher <sgutierrezram@unsa.edu.pe>
2026-05-28 15:34:52 -0500

    Quitando elementos

--- lab06/exercises/MyDjangoProject/MyWebApps/siglas/urls.py
+++ lab06/exercises/MyDjangoProject/MyWebApps/siglas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Al entrar a /listar/ se ejecutará la función de la vista
+  
     path('listar/', views.listar_datos, name='listar_datos'),
 ]
