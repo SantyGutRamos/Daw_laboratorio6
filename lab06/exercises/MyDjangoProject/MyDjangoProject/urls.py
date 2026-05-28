7362d6c13fc3235e30b0a800f9ffde88d824beef
Santiago Cristopher <sgutierrezram@unsa.edu.pe>
2026-05-28 15:32:03 -0500

    Arreglando detalles

--- lab06/exercises/MyDjangoProject/MyDjangoProject/urls.py
+++ lab06/exercises/MyDjangoProject/MyDjangoProject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include  # Asegúrate de importar 'include'
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Enlazamos el archivo urls.py de tu app bajo el prefijo 'proyecto/'
+   
     path('proyecto/', include('MyWebApps.siglas.urls')),
 ]
